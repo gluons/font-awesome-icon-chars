44ffabcaf2eab3de63f78dd9db4d33f4572c947b
Saran Tanpituckpong <sarunta@gmail.com>
2017-01-08 21:53:07 +0700

    TypeScript definition default export

--- definition/font-awesome-icon-chars.d.ts
+++ definition/font-awesome-icon-chars.d.ts
@@ -1,11 +1,13 @@
-interface Icon {
-	id: string;
-	aliases?: string[];
-	unicode: string;
+declare namespace FontAwesomeIconChars {
+	interface Icon {
+		id: string;
+		aliases?: string[];
+		unicode: string;
+	}
 }
 
-declare let faIconChars: Icon[];
+declare let faIconChars: FontAwesomeIconChars.Icon[];
 
 declare module 'font-awesome-icon-chars' {
-	export = faIconChars
+	export default faIconChars;
 }
