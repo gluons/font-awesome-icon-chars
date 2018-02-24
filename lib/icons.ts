61cee33d07740ecd26624d65e58eacdf89641817
Saran Tanpituckpong <sarunta@gmail.com>
2018-02-25 01:35:33 +0700

    :bento: Add regular Font Awesome icons

--- lib/icons.ts
+++ lib/icons.ts
@@ -1,6 +1,8 @@
 /// <reference path='./types.d.ts' />
 
-import solid, { IconDefinition } from '@fortawesome/fontawesome-free-solid';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import regular from '@fortawesome/fontawesome-free-regular';
+import solid from '@fortawesome/fontawesome-free-solid';
 
 /**
  * Get solid icons information.
@@ -11,6 +13,24 @@ import solid, { IconDefinition } from '@fortawesome/fontawesome-free-solid';
 export function getSolidIcons(): IconInfo[] {
 	return Object.keys(solid).map(key => {
 		let iconDef: IconDefinition = solid[key];
+		let iconInf: IconInfo = {
+			name: iconDef.iconName,
+			unicode: iconDef.icon[3] // 4th item of icon is unicode.
+		};
+
+		return iconInf;
+	});
+}
+
+/**
+ * Get regular icons information
+ *
+ * @export
+ * @returns {IconInfo[]}
+ */
+export function getRegularIcons(): IconInfo[] {
+	return Object.keys(regular).map(key => {
+		let iconDef: IconDefinition = regular[key];
 		let iconInf: IconInfo = {
 			name: iconDef.iconName,
 			unicode: iconDef.icon[3] // 4th item of icon is unicode.
