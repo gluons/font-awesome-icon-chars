530c82a4a3e37541b65c2c218eea1d148da39ce5
Saran Tanpituckpong <sarunta@gmail.com>
2018-03-01 14:57:04 +0700

    :recycle: Extract duplicate code

--- lib/icons.ts
+++ lib/icons.ts
@@ -1,17 +1,17 @@
 /// <reference path='./types.d.ts' />
 
-import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+import { IconDefinition, IconPack } from '@fortawesome/fontawesome-common-types';
 import regular from '@fortawesome/fontawesome-free-regular';
 import solid from '@fortawesome/fontawesome-free-solid';
 
 /**
- * Get solid icons information.
+ * Get icons information from icons source.
  *
- * @export
+ * @param {IconPack} iconsSource Icons source
  * @returns {IconInfo[]}
  */
-export function getSolidIcons(): IconInfo[] {
-	return Object.keys(solid).map(key => {
+function getIcons(iconsSource: IconPack): IconInfo[] {
+	return Object.keys(iconsSource).map(key => {
 		let iconDef: IconDefinition = solid[key];
 		let iconInf: IconInfo = {
 			name: iconDef.iconName,
@@ -23,19 +23,21 @@ export function getSolidIcons(): IconInfo[] {
 }
 
 /**
+ * Get solid icons information.
+ *
+ * @export
+ * @returns {IconInfo[]}
+ */
+export function getSolidIcons(): IconInfo[] {
+	return getIcons(solid);
+}
+
+/**
  * Get regular icons information
  *
  * @export
  * @returns {IconInfo[]}
  */
 export function getRegularIcons(): IconInfo[] {
-	return Object.keys(regular).map(key => {
-		let iconDef: IconDefinition = regular[key];
-		let iconInf: IconInfo = {
-			name: iconDef.iconName,
-			unicode: iconDef.icon[3] // 4th item of icon is unicode.
-		};
-
-		return iconInf;
-	});
+	return getIcons(regular);
 }
